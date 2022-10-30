f9b2b233c595e6968ed6ad81c9ed2b5cebfd9981
emre keles <emre.keles@tomdigital.com.tr>
2022-10-30 19:41:19 +0300

    Sourcing API

---
+++ src/Services/Sourcing/Sourcing.API/Controllers/AuctionController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sourcing.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class AuctionController : ControllerBase
+    {
+        private ILogger<AuctionController> _logger;
+
+        public AuctionController(ILogger<AuctionController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
